6cb9f1f81f4e9e4544ac54626e22af2ca782cef2
Nicole Ng <nicoleng123@gmail.com>
2022-09-05 00:24:39 -0400

    personal website created with React

--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import { Squash as Hamburger } from 'hamburger-react';
+import {Squash as Hamburger} from 'hamburger-react';
 
 export default function AboutMe() {
     const [isOpen, setOpen] = useState(false);
@@ -8,46 +8,63 @@ export default function AboutMe() {
         <>
             <div className="navbar">
 
-                    <div id="flex-container">
-                        <div>
-                            <a href="/"><img src="/nn.png" alt='NN' id="initials" /></a>
-                        </div>
-                        <div id="hamburger">
-                            <Hamburger size={20} label="Show menu" toggled={isOpen} toggle={setOpen}/>
-                        </div>
-                            {isOpen && (
-                                <>
-                                    <div id="menu">
-                                        <p><a href="/about-me">More About Me</a></p>
-                                        <p><a href="/">Home</a></p>
-                                    </div>
-                                </>
-                            )}
-
+                <div id="flex-container">
+                    <div>
+                        <a href="/"><img src="/nn.png" alt='NN' id="initials"/></a>
                     </div>
+                    <div id="hamburger">
+                        <Hamburger size={20} label="Show menu" toggled={isOpen} toggle={setOpen}/>
+                    </div>
+                    {isOpen && (
+                        <>
+                            <div id="menu">
+                                <p><a href="/about-me">More About Me</a></p>
+                                <p><a href="/">Home</a></p>
+                            </div>
+                        </>
+                    )}
+
                 </div>
+            </div>
 
-            <h1>More About Me</h1>
-            <h2>
-                Personal Interests
-            </h2>
-            <ul className="interests">
-                <li>
-                    I love social dancing, especially salsa and bachata these days. I used to compete in Ballroom and
-                    Latin dance (all 4 styles!) when I was at NYU.
-                </li>
-                <li>
-                    I’ve always dabbled in arts and crafts growing up. I like painting scenery from places I've
-                    visited as a visual travel diary.
-                </li>
-                <li>
-                    Always learning: I love reading books on many topics like philosophy, psychology, business,
-                    sociology, and more, as well as learning from hearing other people's life stories and experiences.
-                    If you're curious about what I'm reading, here's my recs.
-                </li>
-            </ul>
+            <div className="wrapper">
+                <div id="header">
+                    <h1>More About Me</h1>
+                </div>
+                <main className="page-content">
+                    <h2>
+                        Personal Interests
+                    </h2>
 
+                        <ul className="interests">
+
+                            <li>
+                                I love social dancing, especially salsa and bachata! My experience
+                                comes from random socials when I was in the
+                                <a href="https://www.instagram.com/p/B5i43KDh02S/?hl=en"> NYU Ballroom and Latin Dance
+                                    Team</a>.
+                            </li>
+
+
+                            <li>
+                                Painting is another creative outlet of mine, currently working on painting scenery from
+                                places
+                                I've traveled to as a <a href="https://www.instagram.com/artsncraft.ng/">visual
+                                diary</a>.
+                            </li>
+
+                        </ul>
+
+                    <h2>
+                        Writing
+                    </h2>
+                    <p>
+                        Thoughts still swirling around, will be captured soon!
+                    </p>
+                </main>
+            </div>
         </>
 
-    );
+    )
+        ;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import {useState} from "react";
-import { Squash as Hamburger } from 'hamburger-react';
+import {Squash as Hamburger} from 'hamburger-react';
 
 export default function Index() {
     const [isOpen, setOpen] = useState(false);
@@ -8,33 +8,33 @@ export default function Index() {
         <>
             <Head>
                 <title>Nicole Ng</title>
-                <meta name="description" content="Welcome to my portfolio"/>
+                <meta name="description" content="Welcome to my website"/>
 
             </Head>
 
             {/*</div>*/}
             {/*<div className="w-full sticky top-4 z-30">*/}
 
-                <div className="navbar">
+            <div className="navbar">
 
-                    <div id="flex-container">
-                        <div>
-                            <a href="/"><img src="/nn.png" alt='NN' id="initials" /></a>
-                        </div>
-                        <div id="hamburger">
-                            <Hamburger size={20} label="Show menu" toggled={isOpen} toggle={setOpen}/>
-                        </div>
-                            {isOpen && (
-                                <>
-                                    <div id="menu">
-                                        <p><a href="/about-me">More About Me</a></p>
-                                        <p><a href="/">Home</a></p>
-                                    </div>
-                                </>
-                            )}
-
+                <div id="flex-container">
+                    <div>
+                        <a href="/"><img src="/nn.png" alt='NN' id="initials"/></a>
                     </div>
+                    <div id="hamburger">
+                        <Hamburger size={20} label="Show menu" toggled={isOpen} toggle={setOpen}/>
+                    </div>
+                    {isOpen && (
+                        <>
+                            <div id="menu">
+                                <p><a href="/">Home</a></p>
+                                <p><a href="/about-me">More About Me</a></p>
+                            </div>
+                        </>
+                    )}
+
                 </div>
+            </div>
 
             <div className="wrapper">
                 <div id="header">
@@ -43,31 +43,33 @@ export default function Index() {
                         Hi, I’m Nicole!
                     </h1>
                     <h2>
-                        A data scientist from NYC passionate about health & wellness, tech, and human connection.
+                        A data scientist from NYC passionate about health & wellness, tech, and communities.
                     </h2>
 
                 </div>
                 <main className="page-content">
                     <p>
-                        I currently work at <a href="https://www.ribbonhealth.com/">Ribbon Health</a>, building a
-                        API and provider data platform for care navigators, health plans, and digital health companies.
-                        Previously I’ve worked at <a href="aetna.com">Aetna</a> and <a href="uber.com">Uber</a>.
+                        I currently work at <a href="https://www.ribbonhealth.com/">Ribbon Health</a>, building data
+                        pipelines and models to deliver accurate provider data to power use cases like
+                        care navigation, referrals and more for other companies in the healthcare spaces.
+                        Previously I've worked at <a href="aetna.com">Aetna</a> and <a href="uber.com">Uber</a> running
+                        experiments focused on changing people's behavior.
                         Check out my <a href="https://www.linkedin.com/in/nicolesng/">Linkedin</a> for more details.
                     </p>
 
                     <p>
-                        In the past I've taught a 10-week <a href="https://github.com/nicoleng/bacinsight_19">Intro to Data
-                        Science Workshop</a> at NYU Stern's Business Analytics Club, covering data visualization, statistics,
-                        machine learning, and more, culminating in final group modeling projects. These newfound skills
-                        were then applied in pro-bono consulting engagements
-                        with <a href="https://www.wearpepper.com/">Pepper</a> and <a href="https://sharemeals.org/">Share Meals</a>,
-                        which I helped source and manage.
+                        When I was at NYU, I taught a 10-week <a href="https://github.com/nicoleng/bacinsight_19">Intro
+                        to Data
+                        Science Workshop</a> covering visualization, statistics,
+                        machine learning, and culminating in final group modeling projects. We also worked
+                        with <a href="https://www.wearpepper.com/">Pepper</a> and <a href="https://sharemeals.org/">
+                        Share Meals</a> on pro-bono consulting engagements.
                     </p>
                     <p>
-                        Looking to get into Data Science? Check out this <a href="https://docs.google.com/document/d/1lobKMk8h12alAvC0bNVlfqxLS8amDSgSYkjWQBIlQEc/edit?folder=0ADQs-R7jtf-6Uk9PVA&pli=1">Dictionary</a> I
-                        made covering key topics for interviews and important concepts I encounter on the job.
+                        Looking to get into Data Science? Check out my <a href="https://docs.google.com/document/d/1lobKMk8h12alAvC0bNVlfqxLS8amDSgSYkjWQBIlQEc/edit?folder=0ADQs-R7jtf-6Uk9PVA&pli=1">
+                            Dictionary</a> covering key topics for interviews and important concepts I typically encounter on the job.
                         I use this as a living doc to track my learning, not as a
-                        published product, but I hope by making it public, it can be helpful to others. Notice something is
+                        published product, but I hope by making it public, it can be helpful to others. Notice something
                         wrong or have any feedback? Please feel free to leave a comment!
                     </p>
 
@@ -77,13 +79,13 @@ export default function Index() {
 
             <footer className="site-footer wrapper">
                 <h3>
-                        Contact
-                    </h3>
-                    <p>
-                        Curious about my work? Can I help you in your journey? Please shoot me a message
-                        at <a href="mailto:nicolesng20@gmail.com">nicolesng20@gmail.com</a> or on
-                        twitter <a href="https://twitter.com/nycole_ng">@nycole_ng</a>.
-                    </p>
+                    Contact
+                </h3>
+                <p>
+                    Curious about my work? Can I help you in your journey? Please shoot me a message
+                    at <a href="mailto:nicolesng20@gmail.com">nicolesng20@gmail.com</a> or on
+                    twitter <a href="https://twitter.com/nycole_ng">@nycole_ng</a>.
+                </p>
 
             </footer>
 
