928fa34cb03106c54e9e22d484e5eb393bbe6987
Nicole Ng <nicoleng123@gmail.com>
2022-11-28 03:08:15 -0500

    add metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,12 +4,13 @@ export default function Document() {
     return (
         <Html>
             <Head>
-
+                <meta property="og:title" content="Nicole Ng" />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content="public/nn.png" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Eczar:wght@400;500;600;700;800&family=Inter:wght@300;400;500;600;700&display=swap"
                     rel="stylesheet"
                 />
-                <title>Nicole Ng</title>
                 <meta charSet="utf-8"/>
 
                 {/*<link rel="shortcut icon" type="image/png"*/}
