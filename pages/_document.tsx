d479aa6cd9bf271b10fd9672c24e4feb07715082
Nicole Ng <nicoleng123@gmail.com>
2022-11-28 04:08:40 -0500

    fix icon thumbnail

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ export default function Document() {
                     rel="stylesheet"
                 />
 
-                {/*<link rel="shortcut icon" href="/favicon.ico" />*/}
+                <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
 
             </Head>
             <body>
